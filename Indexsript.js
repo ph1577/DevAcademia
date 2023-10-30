4d9c43a5abc4bdbcbdba2800dd16f49f809837a3
ph1577 <149338789+ph1577@users.noreply.github.com>
2023-10-29 23:00:06 -0300

    First Project

---
+++ Indexsript.js
@@ -0,0 +1,28 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+const sidebar = document.querySelector('.sidebar');
+const barsIcon = document.querySelector('.fa-bars');
+
+
+registerLink.addEventListener('click', () => {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+});
+
+barsIcon.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
